7046713d32f5fa958544c18ff7fa64a94931a89c
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2017-12-12 20:43:38 -0500

    many to many association bw sellers and product lines works

--- app/models/product_line.rb
+++ app/models/product_line.rb
@@ -1,5 +1,6 @@
 class ProductLine < ActiveRecord::Base
 
+  has_many :seller_product_lines
   has_many :sellers, through: :seller_product_lines
   has_many :product_items
 
--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,5 +1,6 @@
 class Seller < ActiveRecord::Base
 
+  has_many :seller_product_lines
   has_many :product_lines, through: :seller_product_lines
   has_many :product_items
 

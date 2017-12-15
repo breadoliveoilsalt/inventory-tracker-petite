8c86ab105a1978ef1d31c2ce7bc6d1cd42701c6b
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2017-12-15 13:09:52 -0500

    Add product line show page

--- app/controllers/product_line_controller.rb
+++ app/controllers/product_line_controller.rb
@@ -1,3 +1,15 @@
 class ProductLinesController < ApplicationController
 
+  get '/product_lines/:id' do
+    if logged_in?
+      @product_line = ProductLine.find(params[:id])
+      erb :'product_lines/show_product_line'
+    else
+      flash[:message] = "**** Please log in first ****"
+      redirect to '/users/login'
+    end
+  end
+
+
+
 end

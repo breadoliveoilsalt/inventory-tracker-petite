cc8dc6a3cd9b73ba3267e5fe5f7421a7fc584c43
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2017-12-15 19:49:23 -0500

    Add in Product Line view the ability to delete a specific inventory item.

--- app/controllers/product_item_controller.rb
+++ app/controllers/product_item_controller.rb
@@ -1,3 +1,18 @@
 class ProductItemsController < ApplicationController
 
+  patch '/product_items/:id' do # patch request to edit a product item
+    item = ProductItem.find(params[:id])
+    item.update(params[:item])
+    item.save
+    flash[:message] = "**** Item \# #{item.id} updated ****"
+    redirect to "/product_lines/#{item.product_line_id}"
+  end
+
+  delete '/product_items/:id/delete' do
+    item = ProductItem.find(params[:id])
+    item.delete
+    flash[:message] = "**** Item \# #{item.id} deleted ****"
+    redirect to "/product_lines/#{item.product_line_id}/edit"
+  end
+
 end
